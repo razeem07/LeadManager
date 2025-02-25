6b9485acf5ea8d19fcc3356400b40a5461715681
Razeem <rrazeem07@gmail.com>
2025-02-25 10:46:43 +0530

    API Crud

---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from api.models import Lead
+
+class LeadSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model=Lead
+
+        fields="__all__"
+
+        read_only_fields=["id","created_date","updated_at"]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+
+from rest_framework.views import APIView
+
+from rest_framework.response import Response
+
+from api.serializers import LeadSerializer
+
+from api.models import Lead
 
 # Create your views here.
+
+class LeadListCreateView(APIView):
+
+    def get(self,request,*args,**kwargs):
+
+        all_leads=Lead.objects.all()
+
+        serializer_instance=LeadSerializer(all_leads,many=True)
+
+        return Response(data=serializer_instance.data)
+    
+
+    def post(self,request,*args,**kwargs):
+
+        serializer_instance=LeadSerializer(data=request.data)
+
+        if serializer_instance.is_valid():
+
+            serializer_instance.save()
+
+            return Response(data=serializer_instance.data)
+        
+        else:
+
+            return Response(data=serializer_instance.errors)
+        
+
+class LeadRetrieveUpdateDestroyView(APIView):
+
+    def get(self,request,*args,**kwargs):
+          
+        id=kwargs.get("pk")
+
+        qs=get_object_or_404(Lead,id=id)
+
+        serializer_instance=LeadSerializer(qs,many=False)  #serialization
+
+        return Response(serializer_instance.data)
+    
+
+    def delete(self,request,*args,**kwargs):
+
+        id=kwargs.get("pk")
+
+        lead_instance=get_object_or_404(Lead,id=id)
+
+        lead_instance.delete()
+
+        return Response(data={"message":"deleted"})
+    
+
+    def put(self,request,*args,**kwargs):
+
+        id=kwargs.get("pk")
+
+        lead_instance=get_object_or_404(Lead,id=id)
+
+        serializer_instance=LeadSerializer(data=request.data,instance=lead_instance)
+
+        if serializer_instance.is_valid():
+
+            serializer_instance.save()
+
+            return Response(data=serializer_instance.data)
+        
+        else:
+
+            return Response(data=serializer_instance.errors)
+
+
+      
+
+
+
+
+
+
